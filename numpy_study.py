afabf692cd4597a02316504f3508173709ae16f0
alt313 <alt313@naver.com>
2020-11-19 23:14:36 +0900

    commit

--- numpy_study.py
+++ numpy_study.py
@@ -719,7 +719,7 @@ print()
 
 # 제곱/제곱근 함수
 # square, sqrt : 제곱, 제곱근 함수
-print('square(), sqrt() : 제곱 제곱근 함수')
+print('square(), sqrt() : 제곱, 제곱근 함수')
 print(a1)
 print(np.square(a1))
 print(np.sqrt(a1))
@@ -767,5 +767,124 @@ print(a2.sum(axis = 0), np.sum(a2, axis = 0))
 print(a2.sum(axis = 1), np.sum(a2, axis = 1))
 print()
 
+# cumsum() : 누적합 계산
+print('cumsum() : 누적합 계산')
+print(a2)
+print(np.cumsum(a2))
+print(np.cumsum(a2, axis = 0))
+print(np.cumsum(a2, axis = 1))
+print()
 
+# diff() : 차분 계산
+print('diff() : 차분 계산')
+print(a2)
+print(np.diff(a2))
+print(np.diff(a2, axis = 0))
+print(np.diff(a2, axis = 1))
+print()
+
+# prod() : 곱 계산
+print('prod() : 곱 계산')
+print(a2)
+print(np.prod(a2))
+print(np.prod(a2, axis = 0))
+print(np.prod(a2, axis = 1))
+print()
+
+# cumprod() : 누적곱 계산
+print('cumprod : 누적곱 계산')
+print(a2)
+print(np.cumprod(a2))
+print(np.cumprod(a2, axis = 0))
+print(np.cumprod(a2, axis = 1))
+print()
+
+# dot() / matmul() : 점곱/행렬곱 계산
+print('dot() / matmul() : 점곱/행렬곱 계산')
+print(a2)
+b2 = np.ones_like(a2)
+print(b2)
+print(np.dot(a2, b2))
+print(np.matmul(a2, b2))
+print()
+
+# tensordot() : 텐서곱 계산
+print('tensordot() : 텐서곱 계산')
+print(a2)
+print(b2)
+print(np.tensordot(a2, b2))
+print(np.tensordot(a2, b2, axes = 0))
+print(np.tensordot(a2, b2, axes = 1))
+print()
+
+# cross() : 벡터곱
+print('cross() : 벡터곱')
+x = [1, 2, 3]
+y = [4, 5, 6]
+print(np.cross(x, y))
+print()
+
+# inner() / outer() : 내적/외적
+print('inner() / outer() : 내적/외적')
+print(a2)
+print(b2)
+print(np.inner(a2, b2))
+print(np.outer(a2, b2))
+print()
+
+# mean() : 평균 계산
+print('mean() : 평균 계산')
+print(np.mean(a2))
+print(np.mean(a2, axis = 0))
+print(np.mean(a2, axis = 1))
+print()
+
+# std() : 표준 편차 계산
+print('std() : 표준 편차 계산')
+print(a2)
+print(np.std(a2))
+print(np.std(a2, axis = 0))
+print(np.std(a2, axis = 1))
+print()
+
+# var() : 분산 계산
+print('var() : 분산 계산')
+print(a2)
+print(np.var(a2))
+print(np.var(a2, axis = 0))
+print(np.var(a2, axis = 1))
+print()
+
+# min() : 최소값
+print('min() : 최소값')
+print(a2)
+print(np.min(a2))
+print(np.min(a2, axis = 0))
+print(np.min(a2, axis = 1))
+print()
+
+# max() : 최대값
+print('max() : 최대값')
+print(a2)
+print(np.max(a2))
+print(np.max(a2, axis = 0))
+print(np.max(a2, axis = 1))
+print()
+
+# argmin() : 최소값 인덱스
+print('argmin() : 최소값 인덱스')
+
+print()
+
+# argmax() : 최대값 인덱스
+print('argmax() : 최대값 인덱스')
+print()
+
+# median() : 중앙값
+print('median() : 중앙값')
+print()
+
+# percentile() : 백분위 수
+print('percentile() : 백분위 수')
+print()
 # =============================================================================
