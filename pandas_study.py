496005a8d4121f161470ad3cd4bbf118b69acf2f
alt313 <alt313@naver.com>
2020-11-25 22:01:34 +0900

    commit

--- pandas_study.py
+++ pandas_study.py
@@ -325,6 +325,7 @@ print()
 
 # 다중 인덱스 생성
 
+print('다중 인덱스 생성')
 df = pd.DataFrame(np.random.rand(6, 3),
                   index = [['a', 'a', 'b', 'b', 'c', 'c'], [1, 2, 1, 2, 1, 2]],
                   columns = ['c1', 'c2', 'c3'])
@@ -350,4 +351,215 @@ print(cols)
 print(data)
 mdf = pd.DataFrame(data, index = idx, columns = cols)
 print(mdf)
+print(mdf['c2'])
+print()
+
+# 인덱싱 및 슬라이싱
+
+print('인덱싱 및 슬라이싱')
+print(population)
+print(population['인천광역시', 2010])
+print(population[:, 2010])
+print(population[population > 3000000])
+print(population[['대구광역시', '대전광역시']])
+print()
+
+print(mdf)
+print(mdf['c2', 1])
+print(mdf.iloc[:3, :4])
+print(mdf.loc[:, ('c2', 1)])
+idx_slice = pd.IndexSlice
+print(mdf.loc[idx_slice[:, 2], idx_slice[:, 2]])
+print()
+
+# 다중 인덱스 재정렬
+
+print('다중 인덱스 재정렬')
+print(korea_mdf)
+korea_mdf = korea_mdf.sort_index()
+print(korea_mdf)
+print(korea_mdf['서울특별시':'인천광역시'])
+print(korea_mdf.unstack(level = 0))
+print(korea_mdf.unstack(level = 1))
+print(korea_mdf.stack())
+print()
+
+idx_flat = korea_mdf.reset_index(level = 0)
+print(idx_flat)
+idx_flat2 = korea_mdf.reset_index(level = (0, 1))
+print(idx_flat2)
+print(idx_flat2.set_index(['행정구역','년도']))
+print()
+# =============================================================================
+
+
+
+
+# 데이터 연산
+# =============================================================================
+
+s = pd.Series(np.random.randint(0, 10, 5))
+print(s)
+df = pd.DataFrame(np.random.randint(0, 10, (3, 3)),
+                  columns = ['A', 'B', 'C'])
+print(df)
+print(np.exp(s))
+print(np.cos(df * np.pi / 4))
+print()
+
+s1 = pd.Series([1, 3, 5, 7, 9], index = [0, 1, 2, 3, 4])
+s2 = pd.Series([2, 4, 6, 8, 10], index = [1, 2, 3, 4, 5])
+print(s1)
+print(s2)
+print(s1 + s2) # index가 없는것 끼리는 계산을 못해서 NaN출력
+print()
+
+# fill_value = 0 옵션 : NaN자리가 있다면 0으로 채움
+print('fill_value = 0 옵션 : NaN자리가 있다면 0으로 채움')
+print(s1.add(s2, fill_value = 0)) 
+print() 
+
+df1 = pd.DataFrame(np.random.randint(0, 20, (3, 3)),
+                   columns = list('ACD'))
+df2 = pd.DataFrame(np.random.randint(0, 20, (5, 5)),
+                   columns = list('BAECD'))
+print(df1)
+print(df2)
+print(df1 + df2)
+fvalue = df1.stack().mean()
+# stack()을 해준후 한줄로 만들어서 평균을 구해서 변수에 할당
+print(fvalue)
+print(df1.add(df2, fill_value = fvalue))
+print()
+
+# 연산자 범용 함수
+
+# add() : 더하기
+print('add() : 더하기')
+a = np.random.randint(1, 10, (3, 3))
+print(a)
+print(a + a[0])
+df = pd.DataFrame(a, columns = list('ABC'))
+print(df)
+print(df + df.iloc[0])
+print(df.add(df.iloc[0]))
+print()
+
+# sub() / subtract() : 빼기
+print('sub() / subtract() : 빼기')
+print(a)
+print(a- a[0])
+print(df)
+print(df - df.iloc[0])
+print(df.sub(df.iloc[0]))
+print(df.subtract(df['B'], axis = 0))
+
+# mul() / multiply() : 곱하기
+print('mul() / multiply() : 곱하기')
+print(a)
+print(a * a[1])
+print(df)
+print(df * df.iloc[1])
+print(df.mul(df.iloc[1]))
+print(df.multiply(df.iloc[2]))
+print()
+
+# truediv() / div() / divide() : 나누기, floordiv() : 몫
+print('truediv() / div() / divide() : 나누기, floordiv() : 몫')
+print(a)
+print(a / a[0])
+print(df)
+print(df / df.iloc[0])
+print(df.truediv(df.iloc[0]))
+print(df.div(df.iloc[0]))
+print(df.divide(df.iloc[0]))
+print(a // a[0])
+print(df.floordiv(df.iloc[0]))
+print()
+
+# mid() : 나머지 
+print('mod() : 나머지')
+print(a)
+print(a % a[0])
+print(df.mod(df.iloc[0]))
+print()
+
+# pow() : 제곱
+print('pow() : 제곱')
+print(a)
+print(a ** a[0])
+print(df)
+print(df.pow(df.iloc[0]))
+print()
+
+
+# 정렬(Sort)
+
+s = pd.Series(range(5), index = ['A', 'D', 'B', 'C', 'E'])
+print(s)
+print(s.sort_index())
+print(s.sort_values())
+print()
+
+df = pd.DataFrame(np.random.randint(0, 10, (4, 4)),
+                  index = [2, 4, 1, 3],
+                  columns = list('BDAC'))
+print(df)
+print(df.sort_index())
+print(df.sort_values(by = 'A')) # A컬럼을 기준으로 정렬
+print(df.sort_values(by = ['A', 'C'])) # A를 기준으로 정렬하고 그다음 C를 정렬
+print(df.sort_index(axis = 1)) # 컬럼을 정렬
+print()
+
+# 순위(Ranking)
+
+# 메소드
+# average : 기본값, 순위에 같은 값을 가지는 항목들의 평균값을 사용
+# min : 같은 값을 가지는 그룹을 낮은 순위로 지정
+# max : 같은 값을 가지는 그룹을 높은 순위로 지정
+# first : 데이터 내의 위치에 따라 순위 지정
+# dense : 같은 그룹 내에서 모두 같은 순위를 적용하지 않고 1씩 증가
+s = pd.Series([-2, 4, 7, 3, 0, 7, 5, -4, 2, 6])
+print(s)
+print(s.rank())
+print(s.rank(method = 'first'))
+print(s.rank(method = 'max'))
+print()
+
+# 고성능 연산
+
+nrows, ncols = 100000, 100
+df1, df2, df3, df4 = (pd.DataFrame(np.random.rand(nrows, ncols)) for i in range(4))
+# %timeit df1 + df2 + df3 + df4
+# %timeit pd.eval('df1 + df2 + df3 + df4')
+# pd.eval을 쓰면 연산이 조금더 빨라진다.
+# %timeit df1 * -df2 / (-df3 * df4)
+# %timeit pd.eval('df1 * -df2 / (-df3 * df4)'
+# %timeit (df1 < df2) & (df2 <= df3) & (df3 != df4)
+# %timeit pd.eval('(df1 < df2) & (df2 <= df3) & (df3 != df4)')
+# 아나콘다 Spyder에서는 되지만 파이참에서는 실행되지 않음
+print()
+
+df = pd.DataFrame(np.random.rand(1000000, 5), columns = ['A', 'B', 'C', 'D', 'E'])
+print(df.head())
+# %timeit df['A'] + df['B'] / df['C'] - df['D'] * df['E']
+# %timeit pd.eval('df.A + df.B / df.C - df.D * df.E')
+# %timeit df.eval('A + B / C - D * E') # 컬럼명만 입력해도 된다.
+df.eval('R = A + B / C - D * E', inplace = True) # 바로 R컬럼을 생성해서 추가한다.
+print(df.head())
+print()
+
+col_mean = df.mean(1)
+print(df['A'] + col_mean)
+print(df.eval('A + @col_mean'))
+# @를 쓰고 변수명을쓰면 외부에 있는 변수를 쓸 수 있다.
+print(df[(df.A < 0.5) & (df.B < 0.5) & (df.C > 0.5)])
+print(pd.eval('df[(df.A < 0.5) & (df.B < 0.5) & (df.C > 0.5)]'))
+print(df.query('(A < 0.5) and (B < 0.5) and (C > 0.5)'))
+print()
+
+col_mean = df['D'].mean()
+print(df[(df.A < col_mean) & (df.B < col_mean)])
+print(df.query('(A < @col_mean) and (B < @col_mean)'))
+print()
 # =============================================================================
