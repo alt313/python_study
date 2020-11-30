e079353d6339c9e70755b511cb3b4131d2ac15af
alt313 <alt313@naver.com>
2020-11-30 23:09:16 +0900

    commit

--- pandas_study.py
+++ pandas_study.py
@@ -324,7 +324,6 @@ print(korea_mdf)
 print()
 
 # 다중 인덱스 생성
-
 print('다중 인덱스 생성')
 df = pd.DataFrame(np.random.rand(6, 3),
                   index = [['a', 'a', 'b', 'b', 'c', 'c'], [1, 2, 1, 2, 1, 2]],
@@ -355,7 +354,6 @@ print(mdf['c2'])
 print()
 
 # 인덱싱 및 슬라이싱
-
 print('인덱싱 및 슬라이싱')
 print(population)
 print(population['인천광역시', 2010])
@@ -373,7 +371,6 @@ print(mdf.loc[idx_slice[:, 2], idx_slice[:, 2]])
 print()
 
 # 다중 인덱스 재정렬
-
 print('다중 인덱스 재정렬')
 print(korea_mdf)
 korea_mdf = korea_mdf.sort_index()
@@ -561,5 +558,214 @@ print()
 col_mean = df['D'].mean()
 print(df[(df.A < col_mean) & (df.B < col_mean)])
 print(df.query('(A < @col_mean) and (B < @col_mean)'))
+print()
+# =============================================================================
+
+
+
+
+
+# 데이터 결합
+# =============================================================================
+
+# concat() / append()
+
+# concat()
+print('concat()')
+s1 = pd.Series(['a', 'b'], index = [1, 2])
+s2 = pd.Series(['c', 'd'], index = [3, 4])
+print(s1)
+print(s2)
+print(pd.concat([s1, s2]))
+print()
+
+# DataFrame concat(세로) axis = 0(기본)
+print('DataFrame concat(세로) axis = 0(기본)')
+def create_df(cols, idx):
+    data = {c: [str(c.lower()) + str(i) for i in idx] for c in cols}
+    return pd.DataFrame(data, idx)
+df1 = create_df('AB', [1, 2])
+df2 = create_df('AB', [3, 4])
+print(df1)
+print(df2)
+print(pd.concat([df1, df2]))
+print()
+
+# DataFrame concat(가로) axis = 1
+print('DataFrame concat(가로) axis = 1')
+df3 = create_df('AB', [0, 1])
+df4 = create_df('CD', [0, 1])
+print(df3)
+print(df4)
+print(pd.concat([df3, df4], axis = 1))
+print()
+
+# index무시하고 새로생성하여 합치기 ignore_index = True
+print('index무시하고 새로생성하여 합치기 ignore_index = True')
+print(pd.concat([df1, df3]))
+print(pd.concat([df1, df3], ignore_index=True))
+print()
+
+# 멀티인덱스 생성하여 합치기 keys = []
+print('멀티인덱스 생성하여 합치기 keys = []')
+print(pd.concat([df1, df3], keys = ['X', 'Y']))
+print()
+
+# join = 'inner' : 둘다 존재하는 데이터만 합친다
+print("join = 'inner' :  둘다 존재하는 데이터만 합친다.")
+df5 = create_df('ABC', [1, 2])
+df6 = create_df('BCD', [3, 4])
+print(df5)
+print(df6)
+print(pd.concat([df5, df6]))
+print(pd.concat([df5, df6], join = 'inner'))
+print()
+
+# append()
+print('append()')
+print(df5.append(df6))
+print()
+
+# 병합과 조인
+
+df1 = pd.DataFrame({'학생' : ['홍길동', '이순신', '임꺽정', '김유신'],
+                    '학과' : ['경영학과', '교육학과', '컴퓨터학과', '통계학과']})
+df2 = pd.DataFrame({'학생' : ['홍길동', '이순신', '임꺽정', '김유신'],
+                    '입학년도' : [2012, 2016, 2019, 2020]})
+print(df1)
+print(df2)
+print()
+
+df3 = pd.merge(df1, df2)
+print(df3)
+print()
+
+df4 = pd.DataFrame({'학과' : ['경영학과', '교육학과', '컴퓨터학과', '통계학과'],
+                    '학과장' : ['황희', '장영실', '안창호', '정약용']})
+print(df4)
+print(pd.merge(df3, df4))
+print()
+
+df5 = pd.DataFrame({'학과' : ['경영학과', '교육학과', '교육학과', '컴퓨터학과', '컴퓨터학과', '통계학과'],
+                    '과목' : ['경영개론', '기초수학', '물리학', '프로그래밍', '운영체제', '확률론']})
+print(df5)
+print(pd.merge(df1, df5))
+print()
+
+df6 = pd.DataFrame({'이름' : ['홍길동', '이순신', '임꺽정', '김유신'],
+                    '성적' : ['A', 'A+', 'B', 'A+']})
+print(df6)
+print(pd.merge(df1, df6, left_on='학생', right_on='이름'))
+print(pd.merge(df1, df6, left_on='학생', right_on='이름').drop('이름', axis = 1))
+print()
+
+mdf1 = df1.set_index('학생')
+mdf2 = df2.set_index('학생')
+print(mdf1)
+print(mdf2)
+print(pd.merge(mdf1, mdf2, left_index = True, right_index = True))
+print(mdf1.join(mdf2))
+print(pd.merge(mdf1, df6, left_index = True, right_on = '이름'))
+print()
+
+df7 = pd.DataFrame({'이름' : ['홍길동', '이순신', '임꺽정'],
+                    '주문음식' : ['햄버거', '피자', '짜장면']})
+df8 = pd.DataFrame({'이름' : ['홍길동', '이순신', '김유신'],
+                    '주문음료' : ['콜라', '사이다', '커피']})
+print(df7)
+print(df8)
+print(pd.merge(df7, df8))
+print(pd.merge(df7, df8, how = 'inner'))
+print(pd.merge(df7, df8, how = 'outer'))
+print(pd.merge(df7, df8, how = 'left'))
+print(pd.merge(df7, df8, how = 'right'))
+print()
+
+df9 = pd.DataFrame({'이름' : ['홍길동', '이순신', '임꺽정', '김유신'],
+                    '순위' : [3, 2, 4, 1]})
+df10 = pd.DataFrame({'이름' : ['홍길동', '이순신', '임꺽정', '김유신'],
+                     '순위' : [4, 2, 3, 1]})
+print(df9)
+print(df10)
+print(pd.merge(df9, df10, on = '이름'))
+print(pd.merge(df9, df10, on = '이름', suffixes = ['_인기', '_성적']))
+print()
+# =============================================================================
+ 
+
+
+
+# 데이터 집계와 그룹 연산
+# =============================================================================
+
+# 집계연산
+
+# count : 전체 개수
+# min, max : 최소값, 최대값
+# cummin, cummax : 누적 최소값, 누적 최대값
+# mean, median : 평균값, 중앙값
+# mad : 절대 평균 편차
+# quantile : 0부터 1까지의 분위수 계산
+# diff : 1차 산술차 계산
+# pct_change : 퍼센트 변화율 계산
+# corr, cov : 상관관계, 공분산 계산
+df = pd.DataFrame([[1, 1.2, np.nan],
+                   [2.4, 5.5, 4.2],
+                   [np.nan, np.nan, np.nan],
+                   [0.44, -3.1, -4.1]],
+                  index = [1, 2, 3, 4],
+                  columns = ['A', 'B', 'C'])
+print(df)
+
+# head, tail : 앞의 항목 일부 반환, 뒤의 항목 일부 반환
+print('head, tail : 앞의 항목 일부 반환, 뒤의 항목 일부 반환')
+print(df.head(2))
+print(df.tail(2))
+print()
+
+# describe : Series, DataFrame의 각 컬럼에 대한 요약 통계
+print('describe : Series, DataFrame의 각 컬럼에 대한 요약 통계')
+print(df)
+print(df.describe())
+
+# argmin, argmax : 최소값과 최대값의 색인 위치
+print('argmin, argmax : 최소값과 최대값의 색인 위치')
+print(df)
+print(np.argmin(df), np.argmax(df))
+print()
+
+# idxmin, idxmax : 최소값과 최대값의 색인값
+print('idxmin, idxmax : 최소값과 최대값의 색인값')
+print(df)
+print(df.idxmin())
+print(df.idxmax())
+print()
+
+# std, var : 표준편차, 분산
+print('std, var : 표준편차, 분산')
+print(df)
+print(df.std())
+print(df.var())
+print()
+
+# skew, kurt : 왜도값 계산, 첨도값 계산
+print('skew, kurt : 왜도값 계산, 첨도값 계산')
+print(df)
+print(df.skew())
+print(df.kurt())
+print()
+
+# sum, cumsum : 전체 항목 합, 누적합
+print('sum, cumsum : 전체 항목 합, 누적합')
+print(df)
+print(df.sum())
+print(df.cumsum())
+print()
+
+# prod, cumprod : 전체 항목 곱, 누적곱
+print('prod, cumprod : 전체 항목 곱, 누적곱')
+print(df)
+print(df.prod())
+print(df.cumprod())
 print()
 # =============================================================================
