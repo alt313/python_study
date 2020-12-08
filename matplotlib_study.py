33eccfba65fae0d95dbbde604e35779bd65358d8
alt313 <alt313@naver.com>
2020-12-08 19:14:15 +0900

    commit

--- matplotlib_study.py
+++ matplotlib_study.py
@@ -429,6 +429,7 @@ plt.scatter(x, y, marker = '.')
 plt.axis('equal')
 plt.annotate('interesting point', xy = (4, 5), xytext = (20, 10),
              arrowprops = dict(shrink = 0.05))
+plt.show()
 print()
 
 # 플롯에 화살표로 가르키는 그림 출력2
@@ -447,6 +448,303 @@ plt.annotate('incorrect value', (40, 30), xytext = (50, 40),
              arrowprops = dict(arrowstyle = '->'))
 plt.show()
 print()
+
+# 눈금 맡춤
+
+# x,y 축을 로그스케일로 출력
+print('x,y 축을 로그스케일로 출력')
+plt.axes(xscale = 'log', yscale = 'log')
+plt.show()
+print()
+
+# NullLocator, NullFormatter : 눈금없음, 눈금레이블 없음
+print('NullLocator, NullFormatter : 눈금없음, 눈금레이블 없음')
+ax = plt.axes()
+ax.plot(np.random.randn(100).cumsum())
+ax.yaxis.set_major_locator(plt.NullLocator())
+ax.xaxis.set_major_formatter(plt.NullFormatter())
+plt.show()
+print()
+
+# MaxNLocator : 눈금 최대 숫자에 맞게 적절한 위치 찾음
+print('MaxNLocator : 눈금 최대 숫자에 맞게 적절한 위치 찾음')
+fig, ax = plt.subplots(3, 3, sharex = True, sharey = True)
+for axi in ax.flat:
+    axi.xaxis.set_major_locator(plt.MaxNLocator(4))
+    axi.yaxis.set_major_locator(plt.MaxNLocator(4))
+fig.show()
+plt.show()
+print()
+
+# 좌표계열 플롯 출력
+print('좌표계열 플롯 출력')
+x = np.linspace(-np.pi, np.pi, 1000, endpoint = True)
+y = np.sin(x)
+plt.plot(x, y)
+ax = plt.gca()
+ax.spines['bottom'].set_position(('data', 0))
+ax.spines['left'].set_position(('data', 0))
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+plt.show()
+print()
+
+# linear스케일과 log스케일 차이
+print('linear스케일과 log스케일 차이')
+x = np.linspace(1, 10)
+y = [10 ** el for el in x]
+z = [2 * el for el in x]
+fig = plt.figure(figsize = (10, 8))
+ax1 = fig.add_subplot(2, 2, 1)
+ax1.plot(x, y, '-y')
+ax1.set_yscale('log')
+ax1.set_title(r'Logarithmic plot of $ {10}^{x} $')
+ax1.set_ylabel(r'$ {y} = {10}^{x} $')
+plt.grid(b = True, which = 'both', axis = 'both')
+
+ax2 = fig.add_subplot(2, 2, 2)
+ax2.plot(x, y, '--r')
+ax2.set_yscale('linear')
+ax2.set_title(r'Linear plot of $ {10}^{x} $')
+ax2.set_ylabel(r'$ {y} = {10}^{x} $')
+plt.grid(b = True, which = 'both', axis = 'both')
+
+ax3 = fig.add_subplot(2, 2, 3)
+ax3.plot(x, y, '-.g')
+ax3.set_yscale('log')
+ax3.set_title(r'Logarithmic plot of $ {2}^{x} $')
+ax3.set_ylabel(r'$ {y} = {2}^{x} $')
+plt.grid(b = True, which = 'both', axis = 'both')
+
+ax4 = fig.add_subplot(2, 2, 4)
+ax4.plot(x, y, ':b')
+ax4.set_yscale('linear')
+ax4.set_title(r'Linear plot of $ {2}^{x} $')
+ax4.set_ylabel(r'$ {y} = {2}^{x} $')
+plt.grid(b = True, which = 'both', axis = 'both')
+plt.show()
+print()
+
+# 스타일
+
+# 플롯 스타일 출력
+print('플롯 스타일 출력')
+fig = plt.figure(figsize = (10, 10))
+x = range(1, 11)
+columns = [np.random.randn(10) * i for i in range(1, 26)]
+
+for n, v in enumerate(plt.style.available[1:]):
+    plt.style.use(v)
+    plt.subplot(5, 5, n + 1)
+    plt.title(v)
+    
+    for c in columns:
+        plt.plot(x, c, marker = '', color = 'royalblue', linewidth = 1, alpha = 0.4)
+        plt.subplots_adjust(hspace = 0.5, wspace = 0.4)
+plt.show()
+print()
+
+# 플롯 스타일 적용
+plt.style.use(['seaborn-notebook'])
+
+# 플롯 종류
+
+# cohere : x와 y의 일관성 시각화 그리기
+# contour : 플롯 등고선
+# errorbar : 오류 막대 그래프
+# hexbin : 육각형 binning 플롯 생성
+# hist: 히스토그램 플롯
+# imshow : 축에 이미지 표시
+# pcolor : 2차원 배열의 유사 플롯 생성
+
+# bar : 막대 플롯 생성
+print('bar : 막대 플롯 생성')
+height = [np.random.randn() * i for i in range(1, 6)]
+names = ['A', 'B', 'C', 'D', 'E']
+y_pos = np.arange(len(names))
+plt.bar(y_pos, height)
+plt.xticks(y_pos, names, fontweight = 'bold')
+plt.xlabel('group')
+plt.show()
+print()
+
+# 막대 플롯 세로로 출력
+print('막대 플롯 세로로 출력')
+height = [np.random.randn() * i for i in range(1, 6)]
+names = ['A', 'B', 'C', 'D', 'E']
+y_pos = np.arange(len(names))
+plt.barh(y_pos, height)
+plt.yticks(y_pos, names, fontweight = 'bold')
+plt.ylabel('group')
+plt.show()
+print()
+
+# 막대그래프 쌓이는 형태
+print('막대그래프 쌓이는 형태')
+bars1 = [12, 28, 1, 8, 22]
+bars2 = [28, 7, 16, 4, 10]
+bars3 = [25, 3, 23, 25, 17]
+bars = np.add(bars1, bars2).tolist()
+
+r = [0, 1, 2, 3, 4]
+names = ['A', 'B', 'C', 'D', 'E']
+
+plt.bar(r, bars1, color = 'royalblue', edgecolor = 'white')
+plt.bar(r, bars2, bottom = bars1, color = 'skyblue', edgecolor = 'white')
+plt.bar(r, bars3, bottom = bars2, color = 'lightblue', edgecolor = 'white')
+
+plt.xlabel('group', fontweight = 'bold')
+plt.xticks(r, names, fontweight = 'bold')
+plt.show()
+print()
+
+
+# 막대그래프 펼쳐지는 형태
+print('막대그래프 펼쳐지는 형태')
+bar_width = 0.25
+
+bars1 = [14, 17, 9, 8, 7]
+bars2 = [14, 7, 12, 4, 10]
+bars3 = [21, 4, 24, 13, 17]
+
+r = [0, 1, 2, 3, 4]
+names = ['A', 'B', 'C', 'D', 'E']
+
+r1 = np.arange(len(bars1))
+r2 = [x + bar_width for x in r1]
+r3 = [x + bar_width for x in r2]
+
+plt.bar(r1, bars1, color = 'royalblue', width = bar_width,edgecolor = 'white', label = r1)
+plt.bar(r2, bars2, color = 'skyblue', width = bar_width, edgecolor = 'white', label = r2)
+plt.bar(r3, bars3, color = 'lightblue', width = bar_width, edgecolor = 'white', label = r3)
+
+plt.xlabel('group', fontweight = 'bold')
+plt.xticks([r + bar_width for r in range(len(bars1))], ['A', 'B', 'C', 'D', 'E'])
+plt.legend()
+plt.show()
+print()
+
+# barbs : barbs의 2차원 필드 그리기
+print('barbs : barbs의 2차원 필드 그리기')
+x = [0, 5, 10, 15,30, 40, 50, 60, 100]
+v = [0, -5, -10, -15, -30, -40, -50, -60, -100]
+n = len(v)
+y = np.ones(n)
+u = np.zeros(n)
+
+plt.barbs(x, y, u, v, length = 9)
+plt.xticks(x)
+plt.ylim(0.98, 1.05)
+plt.show()
+print()
+
+# 스템 플롯
+
+# 줄기모양 플롯출력
+print('줄기모양 플롯출력')
+x = np.linspace(0.1, 2 * np.pi, 41)
+y = np.exp(np.sin(x))
+
+plt.stem(x, y, linefmt = 'gray', bottom = 1, use_line_collection = True)
+plt.show()
+print()
+
+# 박스 플롯
+
+# boxplot : 상자 및 수염 플롯 생성
+print('boxplot : 상자 및 수염 플롯 생성')
+r1 = np.random.normal(loc = 0, scale = 0.5, size = 100)
+r2 = np.random.normal(loc = 0.5, scale = 1, size = 100)
+r3 = np.random.normal(loc = 1, scale = 1.5, size = 100)
+r4 = np.random.normal(loc = 1.5, scale = 2, size = 100)
+r5 = np.random.normal(loc = 2, scale = 2.5, size = 100)
+
+f, ax = plt.subplots(1, 1)
+ax.boxplot((r1, r2, r3, r4, r5))
+ax.set_xticklabels(['r1', 'r2', 'r3', 'r4', 'r5'])
+plt.show()
+# 박스가 안나옴...
+print()
+
+# 산점도
+
+# 산점도 출력
+print('산점도 출력')
+plt.plot(np.random.randn(50), 'o')
+plt.show()
+print()
+
+# 산점도 종류별로 출력
+print('산점도 종류별로 출력')
+plt.figure(figsize = (8, 4))
+markers = ['.', ',', 'o', 'v', '^', '<', '>', '1', '2', '3', '4', 's', 'p', '*', 'h', 'H', '+', 'D', 'd', '|', '_']
+for m in markers:
+    plt.plot(np.random.randn(5), np.random.randn(5), m, label = "'{0}'".format(m))
+plt.legend(loc = 'center right', ncol = 2)
+plt.xlim(0, 1.5)
+plt.show()
+print()
+
+# plot가 아닌 scatter로 산점도 출력
+print('plot가 아닌 scatter로 산점도 출력')
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+plt.scatter(x, y, marker = 'o')
+plt.show()
+print()
+
+# 반복문을 돌려서 산점도 출력
+print('반복문을 돌려서 산점도 출력')
+for i in range(9):
+    x = np.arange(1000)
+    y = np.random.randn(1000).cumsum()
+    plt.scatter(x, y, alpha = 0.2, cmap = 'viridis')
+plt.show()
+print()
+
+# 버블차트로 출력
+print('버블차트로 출력')
+x = np.random.randn(100)
+y = np.random.randn(100)
+colors = np.random.rand(100)
+sizes = 1000 * np.random.rand(100)
+
+plt.scatter(x, y, c = colors, s = sizes, alpha = 0.3, cmap = 'viridis')
+plt.colorbar()
+plt.show()
+print('cmap : color map : https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html')
+print()
+
+# 버블차트 색상 변경
+print('버블차트 색상 변경')
+x = np.random.randn(100)
+y = np.random.randn(100)
+colors = np.random.rand(100)
+sizes = 1000 * np.random.rand(100)
+
+plt.scatter(x, y, c = colors, s = sizes, alpha = 0.3, cmap = 'magma')
+plt.colorbar()
+plt.show()
+print()
+
+# x와 y의 일관성 차트
+
+# 2개의 일관성 출력
+print('2개의 일관성 출력')
+dt = 0.01
+t = np.arange(0, 30, dt)
+n1 = np.random.randn(len(t))
+n2 = np.random.randn(len(t))
+
+s1 = 1.5 * np.sin(2 * np.pi * 10 * t) + n1
+s2 = np.cos(np.pi * t) + n2
+plt.cohere(s1, s2 ** 2, 128, 1./dt)
+plt.xlabel('time')
+plt.ylabel('coherence')
+plt.show()
+print()
+
 
 # =============================================================================
 
